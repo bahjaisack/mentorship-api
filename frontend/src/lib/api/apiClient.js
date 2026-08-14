ad80bf41487cdb5c6d1a188009b15aeb0cf4b6d4
bahjaisack <bahjaisack@gmail.com>
2026-08-14 06:49:03 -0700

    fixing the url

--- frontend/src/lib/api/apiClient.js
+++ frontend/src/lib/api/apiClient.js
@@ -1,22 +1,22 @@
-import axios from 'axios'
-import { config } from 'dotenv';
-import useAuthStore from '../store/authStore';
+import axios from "axios";
+import { config } from "dotenv";
+import useAuthStore from "../store/authStore";
 
-const API_URL = "https://mentorship-api-1-e9zq.onrender.com/api";
-const api =axios.create({
-    baseURL: API_URL,
-    headers: {
-        'Content-Type': "application/json"
-    }
-})
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 // intercepter to add the authorization bearer
 
-api.interceptors.request.use((config)=>{
-    const token = useAuthStore.getState().token;
-    if(token){
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-})
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
