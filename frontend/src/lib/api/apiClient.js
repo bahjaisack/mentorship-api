9d0e71b067baa0208353f61c79a88c9954b7881e
bahjaisack <bahjaisack@gmail.com>
2026-08-14 04:28:03 -0700

    added production url

--- frontend/src/lib/api/apiClient.js
+++ frontend/src/lib/api/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { config } from 'dotenv';
 import useAuthStore from '../store/authStore';
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://mentorship-api-1-e9zq.onrender.com/api";
 const api =axios.create({
     baseURL: API_URL,
     headers: {
